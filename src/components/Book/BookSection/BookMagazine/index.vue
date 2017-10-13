e9e35d80204f07bd32dd94f13fc6d6879fc75bb1
jeven <jevense@qq.com>
2017-10-13 18:11:21 +0800

    init

--- src/components/Book/BookSection/BookMagazine/index.vue
+++ src/components/Book/BookSection/BookMagazine/index.vue
@@ -2,51 +2,61 @@
   <main class="content">
     <header>
       <div class="page-header">
-
+        <span>中国毕业后医学教育</span>
+        <span>2017年第1期第1卷</span>
+        <span>Chinese Journal Of Graduate Medical Education</span>
+        <span>Vol.1 No.1 2017</span>
       </div>
       <hr>
       <div class="category">
-
+        论著
       </div>
       <h1 class="title">
-
+        加强制度建设确保住院医师培训质量的探索
       </h1>
       <div class="author">
-
+        <span>万学红<sub>1*</sub></span>，
+        <span>王星月<sub>2</sub></span>，
+        <span>黄丹丹<sub>2</sub></span>，
+        <span>杨洋<sub>2</sub></span>
       </div>
       <div class="unit">
-
+        （1.四川大学华西医院院办；2.毕业后培训部，四川 成都 610041）
       </div>
       <div class="abstract">
-
+        <strong>摘要：</strong>对华西医院自2000年开展规范化培训的2 976名住院医师的培训、考核和管理进行分析与研究。经验表明：狠抓导师制执行、师资培训与要求、专职教学岗、四部考核体系、科室带教评价与绩效挂钩等制度的建设，有力保证了培训质量。通过实践得出以下结论：基本制度建设在全院达成共识，在规定的时间、规定的地点完成规定的培训任务等制度在各科室严格执行，是保证规范化培训质量的基础；对科室、教师、规培学员的严格管理，对科室和教师带教质量奖惩分明是保证规培质量的有力措施；建立科学合理而规范的学员考核评估体系，并根据结果反馈不断改进培训内容和方法，是不断推进和提高住院医师规培质量的有效策略与办法。
       </div>
       <div class="keyword">
-
+        <strong>关键词：</strong>住院医师培训计划；质量保证；核心措施
       </div>
       <div class="detail">
-
+        <span><strong>中图分类号：</strong>R-4；G642</span>
+        <span><strong>文献标识码：</strong>A</span>
+        <span><strong>文章编号：</strong>2096-4293(2017)01-030-05</span>
       </div>
       <h1 class="title en">
-
+        Quality assurance of residency training program by implementation of core measures
       </h1>
       <div class="author en">
-
+        WAN Xuehong*, WANG Xinyue, HUANG Dandan, YANY Yang
       </div>
       <div class="unit en">
-
+        (Huaxi Hospital, Sichuan University, Chengdu, Sichuan 610041, China)
       </div>
       <div class="abstract en">
-
+        <strong>Abstract:</strong>We summarized the data from 2 976 residents trained at West China Hospital, Sichuan University, from 2000 to 2017, with analysis of the residency program management, education and training quality, and evaluation methods. We established our “Huaxi Experience” with our core measures of residency training programs. Our core measures include endorsement from the department leadership, detailed curriculum, requirement of the duration of each rotation, mentor-trainee system, dedicated clinical teaching faculty position, and 4-step resident evaluation system (daily, monthly, annual, final). The implementation of the core measures plays important roles in quality assurance. We conclude that the detailed curriculum, education and training policies, evaluation and feedback system are crucial for quality assurance of the residency training programs.
+      </div>
+      <div class="keyword en">
+        <strong>Key words:</strong>Residency training program; Quality assurance; Core measures
       </div>
     </header>
     <article>
       <section v-for="(items,index) in section['section']">
-        <div class="section-content" :items="items['section']">
-          <template v-for="item in items['section']">
-            <book-paragraph :item="item"></book-paragraph>
-          </template>
-        </div>
-        <div v-if="index != section['section'].length-1" class="green"></div>
+        <h3>1&nbsp;制度建设</h3>
+        <p>
+          <span>1.1  招录制度</span>
+          人才是卫生事业发展的第一资源，是深化医药卫生体制改革的重要支撑。为培养高质量的卫生人才，四川大学华西医院（华西医院）从2000年开展以社会人身份招收应届毕业生进行规范化培训（规培）[1]，经过17年不断探索实践，已建立起一套较完整的住院医师规培体系。目前已培训社会人身份的住院医师1 727人，外单位委培住院医师1 187人，农村订单定向免费生规培62人。本研究对华西医院在住院医师规培制度建设、严格管理、质量保证体系建设等进行分析与研究，现报道如下。
+        </p>
       </section>
     </article>
     <footer>
